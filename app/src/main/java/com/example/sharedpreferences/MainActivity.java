dc092585e7402f8be338dac94018fa953c03f63c
MohamedMokchaha <72438816+MohamedMokchaha@users.noreply.github.com>
2021-01-02 00:02:21 +0100

    .
    .

---
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+   private static final String TAG = "MainActivity";
+   private SharedPreferences mPreferences;
+   private SharedPreferences.Editor mEditor;
+   private EditText mName, mPassword;
+   private Button btnLogin;
+   private CheckBox mCheckBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mName = (EditText) findViewById(R.id.etName);
+        mPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (button) findViewById(R.id.btnLogin);
+        mChekBox = (CheckBox) findViewById(R.id.checkBox);
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+checkSharedPreferences();
+btnLogin.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        if (mCheckBox.isChecked()) {
+            mEditor.putString(getString(R.string.name), name);
+            mEditor.commit();
+            String password = mPassword.getText().toString();
+            mEditor.putString(getString(R.string.password), password);
+            mEditor.commit();
+        } else {
+            mEditor.putString(getString(R.string.checkbox), "False");
+            mEditor.commit();
+
+            mEditor.putString(getString(R.string.name), "");
+            mEditor.commit();
+
+            mEditor.putString(getString(R.string.password), "");
+            mEditor.commit();
+        }
+
+    }
+
+
+    private void checkSharedPreferences() {
+        String checkbox = mPreferences.getString(getString(R.string.checkbox), "False");
+        String name = mPreferences.getString(getString(R.string.name), "");
+        String password = mPreferences.getString(getString(R.string.password), "");
+        mName.setText(name);
+        mPassword.setText(password);
+
+        if (checkbox.equals("True")) {
+            mCheckBox.setChecked(true);
+        } else {
+            mCheckBox.setChecked(false);
+        }
+    }
+}
